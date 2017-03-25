3189e0e1304bd47f4c594075e6d6ee1d738ee0e3
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-24 23:56:00 -0400

    fix: add proxy_pass from env to nginx

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,13 @@ MAINTAINER "Pete Muir <pmuir@bleepbleep.org.uk>"
 ENV LANG=en_US.utf8
 
 USER root
+
+RUN yum -y install gettext && yum clean all
+
 ADD root /
 
 # Clear out the default config
 RUN rm -rf /etc/nginx/conf.d/default.conf
-
-# Add the templater to run.sh
-RUN sed -i '2s/^/\/template.sh \/usr\/share\/nginx\/html \n/' /run.sh
 
 RUN rm /usr/share/nginx/html/*
 
@@ -20,5 +20,9 @@ RUN useradd --no-create-home -s /bin/bash ${FABRIC8_USER_NAME}
 RUN chmod +rx /run.sh /template.sh
 RUN chmod -R +r /usr/share/nginx/html
 RUN chmod -R +rw /var/log/nginx
+RUN chmod -R a+rw /etc/nginx
+
+# Add the templater to run.sh
+RUN sed -i "2s/^/\/template.sh \/usr\/share\/nginx\/html\n\/template.sh \/etc\/nginx\/nginx.conf \n/" /run.sh
 
 USER ${FABRIC8_USER_NAME}
--- root/template.sh
+++ root/template.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 
-OUTDIR="$@/_config"
-INDIR="$@/config"
-mkdir -p ${OUTDIR}
-
-for infile in $INDIR/*; do
+function template () {
+  infile=${1}
+  OUTDIR=${3}
   tmpfile=${infile}.tmp
-  outfile=${OUTDIR}/`basename ${infile}`
+  if [ -n "${OUTDIR}" ]; then
+    outfile=${OUTDIR}/`basename ${infile}`
+  else
+    outfile=$infile
+    ARGS='$PROXY_PASS_URL'
+  fi
   echo "Templating ${infile} and saving as ${outfile}"
-  sed "s/{{ .Env.\([a-zA-Z0-9_-]*\) }}/\${\1}/" < ${infile} | sed 's/"/\\"/g' > ${tmpfile}
-  eval "echo \"$(cat ${tmpfile})\"" > $outfile
+  sed "s/{{ .Env.\([a-zA-Z0-9_-]*\) }}/\${\1}/" < ${infile} > ${tmpfile}
+  envsubst "$ARGS" < ${tmpfile} > $outfile
   rm ${tmpfile}
   echo ""
   echo "----------------"
   cat $outfile
   echo "----------------"
   echo ""
-done
+}
+
+if [ -f $1 ]; then
+  template $1 $2
+fi
+
+if [ -d $1 ]; then
+  OUTDIR="$1/_config"
+  INDIR="$1/config"
+  mkdir -p ${OUTDIR}
+
+  for infile in $INDIR/*; do
+    template ${infile} $2 ${OUTDIR}
+  done
+fi
+
+
 
 rm -rf $INDIR
+
